038aced2f468ad95355c5bfa854445ef9a38f106
YosefBerger <yosefnberger@gmail.com>
2019-06-18 13:47:43 +0300

    Unfinishe stuff we did in clasd

--- PL_WPF/AddTrainneWindow.xaml.cs
+++ PL_WPF/AddTrainneWindow.xaml.cs
@@ -22,11 +22,31 @@ namespace PL_WPF
         public AddTrainneWindow()
         {
             InitializeComponent();
+            this.genderComboBox.ItemsSource = Enum.GetValues(typeof(BE.Gender));
+            this.gearBoxComboBox.ItemsSource = Enum.GetValues(typeof(BE.GearBox));
+            this.vehicleTypeComboBox.ItemsSource = Enum.GetValues(typeof(BE.VehicleType));
+            BE.Trainee trainee = new BE.Trainee
+            {
+                ID = "1234",
+                GearBox = BE.GearBox.MANUAL
+            };
         }
 
         private void ConfirmBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            trainee = new BE.Trainee
+            {
+                ID = iDTextBox.Text,
+                GearBox = (BE.GearBox)gearBoxComboBox.SelectedItem
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource traineeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("traineeViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // traineeViewSource.Source = [generic data source]
         }
     }
 }
